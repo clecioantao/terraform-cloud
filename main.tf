0abc16d36f1c923a33f76d060befc8048359d9af
Clecio Antao <clecio_antao@carrefour.com>
2024-11-17 09:15:23 -0300

    feat: change name of the resources

--- main.tf
+++ main.tf
@@ -9,21 +9,21 @@ terraform {
 
 provider "azurerm" {
   features {}
-  
+
 
 }
 
 //https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/resource_group
 
-resource "azurerm_resource_group" "appgrp" {
+resource "azurerm_resource_group" "hcp-teste" {
   name     = "app-grp"
   location = "North Europe"
 }
 
 // https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/storage_account
 
-resource "azurerm_storage_account" "appstore40000907819" {
-  name                     = "appstore40000907819"
+resource "azurerm_storage_account" "hcpteste690201" {
+  name                     = "hcpteste690201"
   resource_group_name      = azurerm_resource_group.appgrp.name
   location                 = azurerm_resource_group.appgrp.location
   account_tier             = "Standard"
